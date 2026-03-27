53d3113c040fca52d073818b655b8bf6b79fc403
shashidhar078 <nagunurishashidhar@gmail.com>
2026-03-27 15:33:43 +0530

    understood basic configuration of invoking an llm and understood completion object

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Welcome to GENAI")
+import 'dotenv/config'; 
+import Groq from "groq-sdk";
+
+
+//creating object , since we are taking groq , let the name be groq itself
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY }); 
+
+async function main()
+{
+    const completion=await groq.chat.completions.create({
+        model:"llama-3.3-70b-versatile",
+        messages:[
+            {
+                role:"user",
+                content:"hi"
+            }
+        ]
+    })
+    console.log(completion);
+}
+
+main();
