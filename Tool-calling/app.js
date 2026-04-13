e7d654dce6591d64125c43aa2b3d578f43f76541
Mothkuri-Ranvitha <ranvithamothkuri@gmail.com>
2026-04-13 12:04:55 +0530

    understanding the parameters of tool-calling and exploring all the parameters of tool-calling

--- Tool-calling/app.js
+++ Tool-calling/app.js
@@ -10,18 +10,49 @@ async function main()
 {
     const completion=await groq.chat.completions.create({
         model:"llama-3.3-70b-versatile",
+        temperature:0,
         messages:[
              {
                 role:"system",
-                content:`you are a smart personal assistant,who answer the asked questions`
+                content:`you are a smart personal assistant,who answer the asked questions
+                you have access to the following tools :
+                    "description": "Search for the latest information and realtime data on the internet",
+                1.webSearch({query}:{query:String})  //`
              },
             {
                 role:"user",
                 content:`When apple 16 was launched?`
             }
+        ],
+        // Sample request body with tool definitions and messages
+            tools: [
+            {
+                "type": "function",
+                "function": {
+                    "name": "webSearch",
+                    "description": "Search for the latest information and realtime data on the internet",
+                    "parameters": {
+                    // JSON Schema object
+                    "type": "object",
+                    "properties": {
+                        "query": {
+                        "type": "string",
+                        "description": "search query to perform search on "
+                        },
+                    },
+                    "required": ["query"]
+                    }
+                }
+            }
         ]
     })
-    console.log(completion.choices[0].message.content);
+    tool_choice:'auto'    
+    console.log(completion.choices[0].message);
 }
 
 main();
+
+async function webSearch({query})
+{
+    return "Iphone 16 was launched in the year of 2024."
+}
