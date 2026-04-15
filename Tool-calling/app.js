9f27efd1512d78c3edc3a5ee46d4e068928d7014
shashidhar078 <nagunurishashidhar@gmail.com>
2026-04-15 20:40:20 +0530

    implemented a infinite loop to control overhead of tool-calling

--- Tool-calling/app.js
+++ Tool-calling/app.js
@@ -10,110 +10,114 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main()
 {
-
     let messages=[
-             {
-                role:"system",
-                content:`you are a smart personal assistant,who answer the asked questions
-                you have access to the following tools :
-                    "description": "Search for the latest information and realtime data on the internet",
-                1.webSearch({query}:{query:String})  //`
-             },
-            {
-                role:"user",
-                content:`When apple 16 was launched?`
-            }
-        ]
-
-    const completion=await groq.chat.completions.create({
-        model:"llama-3.3-70b-versatile",
-        temperature:0,
-        // Sample request body with tool definitions and messages
-            messages:messages,
-            tools: [
-            {
-                "type": "function",
-                "function": {
-                    "name": "webSearch",
-                    "description": "Search for the latest information and realtime data on the internet",
-                    "parameters": {
-                    // JSON Schema object
-                    "type": "object",
-                    "properties": {
-                        "query": {
-                        "type": "string",
-                        "description": "search query to perform search on "
-                        },
-                    },
-                    "required": ["query"]
-                    }
+                {
+                    role:"system",
+                    content:`you are a smart personal assistant,who answer the asked questions
+                    you have access to the following tools :
+                        "description": "Search for the latest information and realtime data on the internet",
+                    1.webSearch({query}:{query:String})  //`
+                },
+                {
+                    role:"user",
+                    content:`When apple 16 was launched?`
                 }
-            }
-        ],
-          tool_choice:'auto' 
-    })
-     
-    messages.push(completion.choices[0].message)// to maintain the history that called tool
-    // console.log(JSON.stringify(completion.choices[0].message));
+            ]
 
-    const toolcalls=completion.choices[0].message.tool_calls
 
-if(!toolcalls)
-{
-    console.log(`AI completions : ${completion.choices[0].message.content}`)
-    return; 
-}
-
-    
-
-for(const tool of toolcalls)
-{
-    console.log("tool : ",tool);
-    const functionName=tool.function.name;
-    const functionParam=tool.function.arguments;
-
-    if(functionName=='webSearch')
+    while(true)
     {
-       const result=await webSearch(JSON.parse(functionParam));
-        console.log("tool result : ",result)
-        messages.push({
-        role: "tool",
-        tool_call_id: tool.id,
-        content: result
-        })
-    }
-}
- const completion2=await groq.chat.completions.create({
-        model:"llama-3.3-70b-versatile",
-        temperature:0,
-        messages:messages,
-        // Sample request body with tool definitions and messages
-            tools: [
-            {
-                "type": "function",
-                "function": {
-                    "name": "webSearch",
-                    "description": "Search for the latest information and realtime data on the internet",
-                    "parameters": {
-                    // JSON Schema object
-                    "type": "object",
-                    "properties": {
-                        "query": {
-                        "type": "string",
-                        "description": "search query to perform search on "
+        
+        const completion=await groq.chat.completions.create({
+            model:"llama-3.3-70b-versatile",
+            temperature:0,
+            // Sample request body with tool definitions and messages
+                messages:messages,
+                tools: [
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "webSearch",
+                        "description": "Search for the latest information and realtime data on the internet",
+                        "parameters": {
+                        // JSON Schema object
+                        "type": "object",
+                        "properties": {
+                            "query": {
+                            "type": "string",
+                            "description": "search query to perform search on "
+                            },
                         },
-                    },
-                    "required": ["query"]
+                        "required": ["query"]
+                        }
                     }
                 }
-            }
-        ],
-          tool_choice:'auto' 
-    })
-     
-    
-    console.log(JSON.stringify(completion2.choices[0].message));
+            ],
+            tool_choice:'auto' 
+        })
+        
+        messages.push(completion.choices[0].message)// to maintain the history that called tool
+        // console.log(JSON.stringify(completion.choices[0].message));
 
+        const toolcalls=completion.choices[0].message.tool_calls
+
+    if(!toolcalls)
+    {
+        console.log(`AI completions : ${completion.choices[0].message.content}`)
+        break; 
+    }
+
+        
+
+    for(const tool of toolcalls)
+    {
+        console.log("tool : ",tool);
+        const functionName=tool.function.name;
+        const functionParam=tool.function.arguments;
+
+        if(functionName=='webSearch')
+        {
+        const result=await webSearch(JSON.parse(functionParam));
+            // console.log("tool result : ",result)
+            messages.push({
+            role: "tool",
+            tool_call_id: tool.id,
+            content: result
+            })
+        }
+    }
+    //  const completion2=await groq.chat.completions.create({
+    //         model:"llama-3.3-70b-versatile",
+    //         temperature:0,
+    //         messages:messages,
+    //         // Sample request body with tool definitions and messages
+    //             tools: [
+    //             {
+    //                 "type": "function",
+    //                 "function": {
+    //                     "name": "webSearch",
+    //                     "description": "Search for the latest information and realtime data on the internet",
+    //                     "parameters": {
+    //                     // JSON Schema object
+    //                     "type": "object",
+    //                     "properties": {
+    //                         "query": {
+    //                         "type": "string",
+    //                         "description": "search query to perform search on "
+    //                         },
+    //                     },
+    //                     "required": ["query"]
+    //                     }
+    //                 }
+    //             }
+    //         ],
+    //           tool_choice:'auto' 
+    //     })
+        
+        
+        console.log(JSON.stringify(completion.choices[0].message));
+
+}
 }
 
 
@@ -125,7 +129,7 @@ async function webSearch({query})
 
     const response=await tavilysearch.search(query);
     const finalResult=response.results.map((result)=>result.content).join("\n\n")
-    console.log(finalResult);
+    // console.log(finalResult);
 
     return finalResult;
 }
